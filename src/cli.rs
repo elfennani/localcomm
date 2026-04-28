d070aa4fd7326a9c1024df1eb8e993753d1f624e
elfennani <elfennani2002@gmail.com>
2026-04-28 23:35:15 +0100

    Added mDNS for service broadcasting and discovery. - Added a GitHub Action workflow to build release files for Linux.

--- src/cli.rs
+++ src/cli.rs
@@ -1,13 +1,65 @@
 use crate::localcomm::HelloRequest;
 use crate::localcomm::local_comm_client::LocalCommClient;
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
 
 pub mod localcomm {
     tonic::include_proto!("localcomm");
 }
 
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Optional name to operate on
+    name: Option<String>,
+
+    /// Sets a custom config file
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    Test {
+        /// lists test values
+        #[arg(short, long)]
+        list: bool,
+    },
+    /// Simply prints a name
+    PrintName {
+        name: String,
+        #[arg(long)]
+        age: i8
+    },
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = LocalCommClient::connect("http://[::1]:50051").await?;
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Test { list }) => {
+            if *list {
+                println!("Printing testing lists...");
+            } else {
+                println!("Not printing testing lists...");
+            }
+        }
+        Some(Commands::PrintName { name, age }) => {
+            println!("Your name is {} ({}yo)", name, age);
+        }
+        None => {}
+    }
+
+    let mut client = LocalCommClient::connect("http://localcomm.local:50051").await?;
 
     let request = tonic::Request::new(HelloRequest {
         name: "Nizar".into(),
--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,10 @@
-use tonic::{Request, Response, Status};
-use tonic::transport::Server;
-use localcomm::{HelloReply, HelloRequest};
+use local_ip_address::local_ip;
 use localcomm::local_comm_server::{LocalComm, LocalCommServer};
+use localcomm::{HelloReply, HelloRequest};
+use mdns_sd::{ServiceDaemon, ServiceInfo};
+use tokio::task;
+use tonic::transport::Server;
+use tonic::{Request, Response, Status};
 
 pub mod localcomm {
     tonic::include_proto!("localcomm");
@@ -14,22 +17,69 @@ pub struct LocalCommApp {}
 impl LocalComm for LocalCommApp {
     async fn say_hello(
         &self,
-        request: Request<HelloRequest>, // Accept request of type HelloRequest
-    ) -> Result<Response<HelloReply>, Status> { // Return an instance of type HelloReply
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloReply>, Status> {
         println!("Got a request: {:?}", request);
 
         let reply = HelloReply {
-            message: format!("Hello {}!", request.into_inner().name), // We must use .into_inner() as the fields of gRPC requests and responses are private
+            message: format!("Hello {}!", request.into_inner().name),
         };
 
-        Ok(Response::new(reply)) // Send back our formatted greeting
+        Ok(Response::new(reply))
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
+
+    let addr = "0.0.0.0:50051".parse()?;
     let localcomm = LocalCommApp::default();
+
+    task::spawn(async {
+        let mdns = ServiceDaemon::new().expect("Failed to create daemon");
+
+        let receiver = mdns.monitor().expect("Failed to monitor daemon");
+        task::spawn(async move {
+            while let Ok(event) = receiver.recv() {
+                match event {
+                    mdns_sd::DaemonEvent::Error(error) => {
+                        eprintln!("Daemon error: {error}");
+                    }
+                    event => {
+                        println!("{event:?}");
+                    }
+                }
+            }
+        });
+
+        // Create a service info.
+        // Make sure that the service name: "mdns-sd-my-test" is not longer than the max length limit (15 by default).
+        let service_type = "_localcomm._tcp.local.";
+        let instance_name = "localcomm_instance";
+        let ip = local_ip().unwrap().to_string();
+        let host_name = "localcomm.local.";
+        let port = 5200;
+        let properties = [("property_1", "test"), ("property_2", "1234")];
+
+        println!(
+            "Broadcasting mDNS service ({}) on {}:{} with host name {}",
+            service_type, ip, port, host_name
+        );
+
+        let my_service = ServiceInfo::new(
+            service_type,
+            instance_name,
+            &host_name,
+            ip,
+            port,
+            &properties[..],
+        )
+        .unwrap();
+
+        // Register with the daemon, which publishes the service.
+        mdns.register(my_service)
+            .expect("Failed to register our service");
+    });
 
     println!("LocalComm instance listening on {}", addr);
     Server::builder()
